26017140a969188b11c9f85271e94f43b94e8de8
normpad <normpad48@gmail.com>
2017-09-02 23:40:20 -0600

    Revert "generate a new address for send_transfer to change balance to"
    This reverts commit 8460a5197b07ee5214a8c83641f3fb90018bcd70.

--- tipbot/bot_api.py
+++ tipbot/bot_api.py
@@ -49,7 +49,6 @@ class api:
 
     #---------IOTA API FUNCTIONS--------------#
     def send_transfer(self,addr,amount):
-        new_address = self.get_new_address()
         ret = self.iota_api.send_transfer(
             depth = 3,
             transfers = [
@@ -60,8 +59,7 @@ class api:
                     value = amount,
                 ), 
             ],
-            min_weight_magnitude=15,
-            change_address = new_address
+            min_weight_magnitude=15
         )
         return ret
 
