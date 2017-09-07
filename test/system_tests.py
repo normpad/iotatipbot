dc1c36fe60b91086b7d4450aeebfe6ca6b9353d0
normpad <normpad48@gmail.com>
2017-09-07 10:40:21 -0600

    update system test

--- test/system_tests.py
+++ test/system_tests.py
@@ -20,10 +20,10 @@ class SystemTests(unittest.TestCase):
         Send 100 transfers and verify that each one is properly received
         """
         def test_systems(self):
-            bot_api_send = api(b'SWMS9HOIE9EKPKWTBKJHTJOX9EU9NNJB9AEDWZPFOBSBQBXBFPYSRCE9SVIHRI9BJAGLSCAMMRUFSQRIJ')
-            bot_api_receive = api(b'DHHJILKLLZBOCQNDMDWETDNMKEBJPAAYM9QNKAVWCEE99DSZXMNHUSWWIPJGUHUGZXEMEJLADOYJPRYSY')
-            bot_db_send = Database('system_test_db1.db')
-            bot_db_receive = Database('system_test_db2.db')
+            bot_api_send = api(b'LMFWBJLBIGZZXYSHXIO9PAUVQZ9AYQBRT9ERSUQEDRAGMILTQKXOSVYJFNOMRSFYGRMFFMLF9CUA9FNZS')
+            bot_api_receive = api(b'PYBTQIJYAYYSO9LLXCWKC9QDIJNWUGJWBGTUNVMFVRTCNWYZRRDEZCMPYCLOYCBGHUIITKPDR9YRCJHKY')
+            bot_db_send = Database('test_db1_send.db')
+            bot_db_receive = Database('test_db_receive.db')
             for i in range(1,100):
                 starting_balance = bot_api_receive.get_account_balance(bot_db_receive.get_address_index())
                 print('Starting Balance: {0}'.format(starting_balance))
@@ -38,13 +38,15 @@ class SystemTests(unittest.TestCase):
                 address_index = bot_db_send.get_address_index()
                 new_address = bot_api_send.get_new_address(address_index)
                 bot_db_send.add_used_address(address_index,address._trytes.decode("utf-8"))
+
                 transaction = bot_api_send.send_transfer(address,i,new_address,address_index)
                 print('{0} sent to address.'.format(i))
+                print(transaction['bundle'].hash)
+                print(transaction['bundle'].tail_transaction.hash)
                 confirmed = False
                 start_time = time.time()
                 while not confirmed:
                     confirmed = bot_api_send.check_transaction(transaction)
-                    print(confirmed)
                     if (time.time() - start_time) > (60) and not confirmed:
                         print('Replaying Bundle')
                         #confirmed = True
