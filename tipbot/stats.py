9cea6aacde5139bc20148dd525fc8962a9292968
normpad <normpad48@gmail.com>
2017-09-13 14:03:34 -0600

    add stats for deposit and withdraw requests

--- tipbot/stats.py
+++ tipbot/stats.py
@@ -3,7 +3,13 @@ from bot_api import api
 fake_seed = 'A'*81
 
 tip_string = re.compile('(\w*) has tipped (\w*) ([0-9]+) iota',re.I)
+withdraw_string = re.compile('(\w*) withdrew (\w*)',re.I)
+deposit_string = re.compile('(\w*) was assigned to address (\w*)',re.I)
+deposit_complete_string = re.compile('(\w*) deposited ([0-9]+) iota',re.I)
 tips = 0
+withdraws = 0
+deposits = 0
+completed_deposits = 0
 tip_average = 0
 bot_api = api(fake_seed)
 
@@ -13,8 +19,22 @@ with open('transactionLogs.log','r') as f:
         if match:
             tips = tips + 1    
             tip_average = tip_average + int(match.group(3))
+        match = withdraw_string.search(line)
+        if match:
+            withdraws = withdraws + 1
+        match = deposit_string.search(line)
+        if match:
+            deposits = deposits + 1
+        match = deposit_complete_string.search(line)
+        if match:
+            completed_deposits = completed_deposits + 1
 
 tip_average = int(tip_average/tips)
 tip_average_value = bot_api.get_iota_value(tip_average)
+completed_deposit_ratio = int((completed_deposits / deposits)*100)
 print('Total tips: {0}'.format(tips))
 print('Average tip: {0}(${1})'.format(tip_average,tip_average_value))
+print('Total withdraws: {0}'.format(withdraws))
+print('Total deposits requests: {0}'.format(deposits))
+print('Total completed deposits: {0}'.format(completed_deposits))
+print('Completed deposit ratio: {0}%'.format(completed_deposit_ratio))
