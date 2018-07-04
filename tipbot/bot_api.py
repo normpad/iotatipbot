2b2c10dc8b05903dcc27417889a5675ae3ea956c
normpad <normpad48@gmail.com>
2018-07-04 17:49:48 -0400

    That shouldn't be commented out

--- tipbot/bot_api.py
+++ tipbot/bot_api.py
@@ -75,7 +75,6 @@ class api:
             try:  
                 inputs = []
                 inputs_total = 0
-                """
                 for i in range(self.starting_input, index):
                     input_addr = self.iota_api.get_new_addresses(i,1)['addresses'][0]
                     input_addr_balance = self.get_balance(input_addr)
@@ -86,7 +85,6 @@ class api:
                         if inputs_total >= amount:
                             print('Inputs found. Total: {0}'.format(inputs_total))
                             break
-                """
                 ret = self.iota_api.send_transfer(
                     depth = 3,
                     transfers = [
